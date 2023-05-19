88d4c909ccf1240cd3b2af48e3f769ca27d5a3b0
Victor Garcia <vgarcia@aidacanarias.com>
2023-05-19 14:30:27 +0200

    Chore: Initial files

---
+++ StringCalculator/StringCalculator.Tests/StringCalculatorShould.cs
@@ -0,0 +1,16 @@
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
