e3b08fdc7897bd74b657f06993f20d2f6acb94c8
Victor Garcia <vgarcia@aidacanarias.com>
2023-05-19 15:07:25 +0200

    Feat: Allow new delimitators

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -12,12 +12,21 @@ namespace StringCalculatorKata
         {
             if (numbers == "") return 0;
 
+            var defaultDelimiters = new char[] { ',', '\n' };
+
+            if (numbers.StartsWith("\\"))
+            {
+                var splitNumbers = numbers.Split('\n');
+                defaultDelimiters = splitNumbers[0].Substring(1, 1).ToCharArray();
+                numbers = splitNumbers[1];
+            }
+
             var numbersList = numbers.ToCharArray();
             var sum = 0;
 
             foreach (var number in numbersList)
             {
-                if(number == ',' || number == '\n') continue;
+                if(defaultDelimiters.Any(d => d == number)) continue;
                 sum += int.Parse(number.ToString());
             }
 
