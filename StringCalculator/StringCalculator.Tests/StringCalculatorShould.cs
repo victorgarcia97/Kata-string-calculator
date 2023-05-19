9607c6d0b2643c0b9b74f1d0a48be4aaa0978a97
Victor Garcia <vgarcia@aidacanarias.com>
2023-05-19 14:47:07 +0200

    Test: Add unilimited numbers in input

--- StringCalculator/StringCalculator.Tests/StringCalculatorShould.cs
+++ StringCalculator/StringCalculator.Tests/StringCalculatorShould.cs
@@ -20,7 +20,11 @@ namespace StringCalculatorTests
 
         [Theory]
         [TestCase("1,2", 3)]
+        [TestCase("2,2", 4)]
         [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
         public void ReturnSumOfInputNumbers(string numbers, int expectedSum)
         {
             var result = StringCalculator.Add(numbers);
